982260136432d8de091126ebb2344d6ae904537b
seema <seemagreat.911@gmail.com>
2021-01-05 12:25:39 -0800

    done

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path')
 const bodyParser= require('body-parser');
+const{resolveSoa} = require('dns')
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.static(path.join(__dirname,'public')))
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -14,48 +15,45 @@ let users=[
     {name:'bisma',id:1,email:'bismatouseeq2020@gmail.com',password:'222'},
     
 ]
+app.use(express.static(path.join(__dirname,'public')))
 
-app.get("/", (req,res)=>{
+app.get('/signup', (req,res)=>{
     // res.send('<h2>AboutUs<h2>')
-      res.sendFile(path.join(__dirname, 'public','index.html'))
+      res.sendFile(path.join(__dirname, 'registration','sign.html'))
 })
 
-app.get("/signup", (req,res)=>{
-    // res.send('<h2>AboutUs<h2>')
-      res.sendFile(path.join(__dirname, 'registration','signup.html'))
-})
 app.post('/signup',(req,res)=>{
-    // res.send(req.body)
-   // console.log(req.body)
-   let {email, name, password} = req.body
-   let found = users.some((item)=>item.email == email)
-   if(found){
-    //    res.send('<h1>User already existed</h1>')
-    res.redirect('/login')
-   } else{
-       users.push({name,email,password,id:users.length+1})
-      // res.send('user added')
-      res.sendFile(path.join(__dirname, 'registration','login.html'))
-   }
- 
- })
-
-app.get("/login", (req,res)=>{
-    // res.send('<h2>AboutUs<h2>')
-    res.sendFile(path.join(__dirname, 'registration','login.html'))
+    let {email, name, password} = req.body
+    let found = users.some((item)=>item.email == email)
+    let found1 = users.some((item)=>item.password == password)
+    if(found && found1){
+     //    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+     res.redirect('/login')
+    } else{
+        users.push({name,email,password,id:users.length+1})
+       res.sendFile(path.join(__dirname, 'public','index.html'))
+     
+    }
+  
+  })
+  app.get('/welcome', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'public', 'welcome.html'))
+})
+app.get('/login', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'registration', 'login.html'))
 })
 app.post('/login',(req,res)=>{
-    // res.send(req.body)
-   // console.log(req.body)
-   let {email, name, password} = req.body
-   let found = users.some((item)=>item.email == email)
-   if(found){
-    //    res.send('<h1>User already existed</h1>')
-    res.redirect('/')
-   } else{
-       users.push({name,email,password,id:users.length+1})
-    //   res.sendFile(path.join(__dirname, 'registration','signup.html'))
-    res.redirect('/')
-   }
- 
- })
+    let {email, name, password} = req.body
+    let found = users.some((item)=>item.email == email)
+    let found1 = users.some((item)=>item.password == password)
+    if(found && found1){
+        // res.sendFile(path.join(__dirname, 'public', 'index.html'))
+        res.redirect('/welcome')
+    } else{
+    //    res.sendFile(path.join(__dirname, 'registration','signup.html'))
+    res.send('<h1>Please Enter Correct Password</h1>')
+    //    users.push({name,email,password,id:users.length+1})
+    }
+  
+  })
+
